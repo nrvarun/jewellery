b915044ab0b6dc17ca45f6ad302a58c69d2d5076
nrvarun <nr.varun79@gmail.com>
2017-09-18 01:12:24 +0530

    new sections added

--- src/js/slick-carousel.js
+++ src/js/slick-carousel.js
@@ -4,17 +4,28 @@ let $ = require('jquery');
 
 $(document).ready(function () {
   $('.sl-intro-banner__slider').slick({
-      dots: true,
-      arrows: true,
-      infinite: true,
-      speed: 600,
-      slidesToShow: 1
-    });
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 1
+  });
+
+  // Platinum Collections slider
   $('.collection-slider').slick({
-      dots: true,
-      arrows: true,
-      infinite: true,
-      speed: 600,
-      slidesToShow: 1
-    });
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 1
+  });
+
+  // Featured Collections slider
+  $('.featured-slider').slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 1
+  });
 });
